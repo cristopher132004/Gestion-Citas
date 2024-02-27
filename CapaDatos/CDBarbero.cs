26d24553e169c1533587afeaa9e5ae2cf8084e47
Cristopher L <cristopherlantigua1233@gmail.com>
2024-02-26 21:56:21 -0400

    Realice CAPA DATOS BARBERO

---
+++ CapaDatos/CDBarbero.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data.Sql;
+
+namespace CapaDatos
+{
+    class CDBarbero
+    {
+        private int dIdBarbero;
+        private String dNombre, dApellido, dTelefono, dEstado;
+
+        public CDBarbero() { }
+        public CDBarbero(int pIdBarbero, string pNombre, string pApellido, string pTelefono, string pEstado)
+        {
+            this.dIdBarbero = pIdBarbero;
+            this.dNombre = pNombre;
+            this.dApellido = pApellido;
+            this.dTelefono = pTelefono;
+            this.dEstado = pEstado;
+        }
+        public int IdBarbero
+        {
+            get { return dIdBarbero; }
+            set { dIdBarbero = value; }
+        }
+
+        public string Nombre
+        {
+            get { return dNombre; }
+            set { dNombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return dApellido; }
+            set { dApellido = value; }
+        }
+
+        public string Telefono
+        {
+            get { return dTelefono; }
+            set { dTelefono = value; }
+        }
+
+        public string Estado
+        {
+            get { return dEstado; }
+            set { dEstado = value; }
+        }
+
+        public string Insertar(CDBarbero objBarbero)
+        {
+            string mensaje = "";
+            //creamos un nuevo objeto de tipo SqlConnection
+            SqlConnection sqlCon = new SqlConnection();
+            //trataremos de hacer algunas operaciones con la tabla
+            try
+            {
+                //asignamos a sqlCon la conexión con las base de datos a traves de la clase que creamos
+                sqlCon.ConnectionString = ConexionDB.miconexion;
+                //Escribo el nombre del procedimiento almacenado que utilizaré, en este caso SuplidorInsertar
+                SqlCommand micomando = new SqlCommand("BarberoInsectar", sqlCon);
+                sqlCon.Open(); //Abro la conexión
+                               //indico que se ejecutara un procedimiento almacenado
+                micomando.CommandType = CommandType.StoredProcedure;
+                micomando.Parameters.AddWithValue("@pNombre", objBarbero.Nombre);
+                micomando.Parameters.AddWithValue("@pApellido", objBarbero.Apellido);
+                micomando.Parameters.AddWithValue("@pTelefono", objBarbero.Telefono);
+                micomando.Parameters.AddWithValue("@pEstado", objBarbero.Estado);
+                //Metodo Insertar
+                mensaje = micomando.ExecuteNonQuery() == 1 ? "Datos actualizados correctamente!" :
+                                                             "No se pudo actualizar correctamente los datos!";
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                    sqlCon.Close();
+            }
+            return mensaje;
+
+        }//Metodo
+         //método para actualizar los datos del Suplidor. Recibirá el objeto objSuplidor como parámetro
+        public string Actualizar(CDBarbero objBarbero)
+        {
+            string mensaje = "";
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon.ConnectionString = ConexionDB.miconexion;
+                SqlCommand micomando = new SqlCommand("BarberoActualizar", sqlCon);
+                sqlCon.Open();
+                micomando.CommandType = CommandType.StoredProcedure;
+                micomando.Parameters.AddWithValue("@pNombre", objBarbero.Nombre);
+                micomando.Parameters.AddWithValue("@pApellido", objBarbero.Apellido);
+                micomando.Parameters.AddWithValue("@pTelefono", objBarbero.Telefono);
+                micomando.Parameters.AddWithValue("@pEstado", objBarbero.Estado);
+                mensaje = micomando.ExecuteNonQuery() == 1 ? "Datos actualizados correctamente!" :
+                 "No se pudo actualizar correctamente los datos!";
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                    sqlCon.Close();
+            }
+            return mensaje;
+        }
+
+        //Método para consultar datos filtrados de la tabla. Se recibe el valor del parámetro
+        public DataTable BarberoConsultar(String miparametro)
+        {
+            DataTable dt = new DataTable(); //Se Crea DataTable que tomará los datos de los Suplidores
+            SqlDataReader leerDatos; //Creamos el DataReader
+            try
+            {
+                SqlCommand sqlCmd = new SqlCommand(); //Establecer el comando
+                sqlCmd.Connection = new ConexionDB().dbconexion; //Conexión que va a usar el comando
+                sqlCmd.Connection.Open(); //Se abre la conexión
+                sqlCmd.CommandText = "BarberoConsultar"; //Nombre del Proc. Almacenado a usar
+                sqlCmd.CommandType = CommandType.StoredProcedure; //Se trata de un proc. almacenado
+                sqlCmd.Parameters.AddWithValue("@pvalor", miparametro); //Se pasa el valor a buscar
+                leerDatos = sqlCmd.ExecuteReader(); //Llenamos el SqlDataReader con los datos resultantes
+                dt.Load(leerDatos); //Se cargan los registros devueltos al DataTable
+                sqlCmd.Connection.Close(); //Se cierra la conexión
+            }
+            catch (Exception ex)
+            {
+                dt = null; //Si ocurre algun error se anula el DataTable
+            }
+            return dt; ////Se retorna el DataTable segun lo ocurrido arriba
+        } //Fin del método MostrarConFiltro
+
+
+    }
+}//Fin de la clase CDCliente
